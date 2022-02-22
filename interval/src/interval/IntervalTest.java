72f964adec5973618479eacb16a1aa2b0fcfcbf2
senne <senne@192.168.0.209>
2022-02-22 17:26:21 +0100

    Initiële versie

---
+++ interval/src/interval/IntervalTest.java
@@ -0,0 +1,32 @@
+package interval;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class IntervalTest {
+	
+	// Verschillende voorstellingen voor hezelfde idee
+	// Verschillende voorstellingen voor eenzelfde abstracte toestandsruimte
+	class Interval{
+		int ondergrens;
+		int bovengrens;
+	}
+	
+	int getLengte(Interval interval) {
+		return interval.bovengrens - interval.ondergrens;
+	}
+	
+	@Test
+	void test() {
+		Interval mijnInterval = new Interval();
+		assertEquals(0, mijnInterval.ondergrens);
+		assertEquals(0, mijnInterval.bovengrens);
+		
+		mijnInterval.bovengrens = 2;
+		assertEquals(2, mijnInterval.bovengrens);
+		
+		assertEquals(2, getLengte(mijnInterval));
+	}
+
+}
